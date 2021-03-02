2461c1c7aacf7b470ddc4508aee0b06ae18ca407
Matheus <mac@MacBook-Pro-de-mac.local>
2021-03-02 18:58:45 -0300

    primeiro commit

---
+++ Shared/PrimeiroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeiroAppApp.swift
+//  Shared
+//
+//  Created by Matheus on 02/03/21.
+//
+
+import SwiftUI
+
+@main
+struct PrimeiroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
